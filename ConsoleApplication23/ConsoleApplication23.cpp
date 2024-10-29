4cefd56d957075a1e8b7cf92112c3765760013fb
Егор <@DESKTOP-VF4U8Q2>
2024-10-29 16:21:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication23/ConsoleApplication23.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    float fileSize, internetSpeed;
+
+    cout << "Укажите размер файла для скачивания: ";
+    cin >> fileSize;
+    cout << "Какова скорость вашего соединения? ";
+    cin >> internetSpeed;
+
+    float downloaded = 0;
+    int sec = 0;
+
+    while (downloaded < fileSize) {
+        downloaded += internetSpeed;
+        sec++;
+        int percent = (int)((downloaded / fileSize) * 100);
+
+        if (percent > 100) {
+            percent = 100;
+        }
+
+        cout << "Прошло " << sec << " сек. Скачано " << downloaded << " из " << fileSize << " МБ (" << percent << "%)." << endl;
+    }   
+}
+
